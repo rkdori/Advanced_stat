9b6a11a56de7eeefebb9e00a5c64844b6859b880
Jiwon <kang020227@gmail.com>
2025-10-19 18:59:57 +0900

    update R scripts

--- R/assignment1.R
+++ R/assignment1.R
@@ -1,8 +1,25 @@
+## Problem 1
 set.seed(1)
 n = 200
 x = seq(0, 1, length.out = n)
 y = sin(2*pi*x) + rnorm(n, sd = 0.15)
 
-data <- data.frame(x = x, y = y)
 
-write.csv(data, "data/assi.csv", row.names = FALSE)
+# 1 - (a)
+data <- data.frame(x = x, y = y)
+write.csv(data, "data/assignment1_data.csv", row.names = FALSE)
+
+# 1 - (b)
+library(ggplot2)
+data1 <- read.csv("data/assignment1_data.csv")
+
+plot1 <- ggplot(data1, aes(x = x, y = y)) +
+  geom_point() +
+  geom_smooth(method = "loess")
+print(plot1)
+
+# 1 - (c)
+ggsave("plots/assignment1_plot1.png", plot = plot1)
+
+# 1 - (d)
+# https://github.com/rkdori/Advanced_stat
